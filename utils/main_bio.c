658a722655de0734158220b6935b34d5f3348520
youri <y.hoogstrate@gmail.com>
2023-09-15 20:11:02 +0200

    sav

--- utils/main_bio.c
+++ utils/main_bio.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "config.h"
+
+
+void usage_build_details()
+{
+	printf("bio v" BIOKERNEL_VERSION "\n");
+	printf("build: " COMPILE_DATE  "\n");
+}
+
 
 void usage_bio()
 {
@@ -10,6 +19,8 @@ void usage_bio()
 	printf("  ls          List information about the file with taxonomy info\n");
 	printf("  chtax       Change taxonomy attribute\n");
 	printf("  rmtax       Remove taxonomy attribute\n");
+	printf("\n");
+	printf("  --version   Shows version and build info\n");
 	printf("\n");
 	printf("Run 'bio COMMAND --help' for more information on a command.\n");
 
@@ -25,7 +36,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		//
+		usage_build_details();
 	}
 	
 	
