c4f347b365ab5d9dab56450d5efd6c77b31ae7e5
youri <y.hoogstrate@gmail.com>
2024-03-12 16:05:45 +0100

    sav

--- utils/main_bio.c
+++ utils/main_bio.c
@@ -16,11 +16,18 @@ void usage_bio()
     printf("Usage: bio [COMMAND]\n");
     printf("\n");
     printf("Commands:\n");
-    printf("  ls          List information about the file with taxonomy info\n");
-    printf("  chtax       Change taxonomy attribute\n");
-    printf("  rmtax       Remove taxonomy attribute\n");
+    printf("  info           Small user guide\n");
+    printf("  ls             List file/direftory information with bio/taxonomy data\n");
+    printf("  list           List cached bio-kernel reference data\n");
+    printf("  chtax          Change taxonomy attribute\n");
+    printf("  rmtax          Remove taxonomy attribute\n");
+    printf("  chref          Change reference sequence (e.g. hg19, GRCh37.p1, GRCh37.p2)\n");
+    printf("  rmref          Change reference sequence (e.g. hg19, GRCh37.p1, GRCh37.p2)\n");
+    printf("  -v, --version  Shows version and build info\n");
     printf("\n");
-    printf("  --version   Shows version and build info\n");
+    printf("Extended command list:\n");
+    printf("  chbuild        Change reference build (e.g. hg18, hg19, GRCh37)\n");
+    printf("  rmbuild        Change reference build (e.g. hg18, hg19, GRCh37)\n");
     printf("\n");
     printf("Run 'bio COMMAND --help' for more information on a command.\n");
 
