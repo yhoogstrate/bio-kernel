8f3386cbea946d604c460ea8a2d4f2e20f61daa3
yhoogstrate <y.hoogstrate@gmail.com>
2026-02-18 12:33:57 +0100

    patch

--- src/lib/tax.c
+++ src/lib/tax.c
@@ -25,12 +25,19 @@ int chtax(char* filename, char* taxon)
     i = removexattr(filename, "user.taxon");
     i = setxattr(filename, "user.taxon", taxon, 16, 0);
     return i;
+
 }
 
 int rmtax(char* filename)
 {
     // @todo only overwrite with --force enabled
-    int i;
-    i = removexattr(filename, "user.taxon");
-    return i;
+    if(filename == NULL) {
+        return -1;
+    }
+    
+    int result;
+    
+    result = removexattr(filename, "user.taxon");
+    
+    return result;
 }
